90808f5ef3865e6b992eb0a8147db78f20836f91
wangyang <657176605@qq.com>
2021-11-12 18:16:36 +0800

    ++

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
-cd ui
-npm run build
-cd ..
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -tags=jsoniter -ldflags "-s -w" -o ./bin/hpa ./main.go
-docker build -t hellojqk/http-proxy-analysis:latest .
-docker push hellojqk/http-proxy-analysis:latest
+# cd ui
+# npm run build
+# cd ..
+# CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -tags=jsoniter -ldflags "-s -w" -o ./bin/hpa ./main.go
+# docker build -t hellojqk/http-proxy-analysis:latest .
+# docker push hellojqk/http-proxy-analysis:latest
 
-version=0.0.9
+version=0.0.10
 docker tag hellojqk/http-proxy-analysis:latest hellojqk/http-proxy-analysis:${version}
 docker push hellojqk/http-proxy-analysis:${version}
