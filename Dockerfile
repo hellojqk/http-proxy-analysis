56b780f99023be720e46bbed0a09c12fbedca739
wangyang <657176605@qq.com>
2021-11-12 18:03:27 +0800

    ++

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ FROM hellojqk/alpine:3.12.0
 WORKDIR /app
 
 COPY bin/hpa /app/hpa
-COPY config /app/config/
+# COPY config /app/config/
 COPY assets /app/assets/
 COPY ui/dist /app/ui/dist
 
--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -tags=jsoniter -ldflags "-s -w" -
 docker build -t hellojqk/http-proxy-analysis:latest .
 docker push hellojqk/http-proxy-analysis:latest
 
-version=0.0.8
+version=0.0.9
 docker tag hellojqk/http-proxy-analysis:latest hellojqk/http-proxy-analysis:${version}
 docker push hellojqk/http-proxy-analysis:${version}
